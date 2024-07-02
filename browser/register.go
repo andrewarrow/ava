de43b0d56162c87b1d6c403ce988a9e5f4bd09b5
Andrew Arrow <oneone@gmail.com>
2024-07-01 20:05:11 -0700

    fixed a bug

---
+++ browser/register.go
@@ -0,0 +1,24 @@
+package browser
+
+import (
+	"github.com/andrewarrow/feedback/wasm"
+)
+
+var Global *wasm.Global
+var Document *wasm.Document
+
+func RegisterEvents() {
+	afterRegister := func(id int64) {
+		Global.Location.Set("href", "/ava/start")
+	}
+	afterLogin := func(id int64) {
+		Global.Location.Set("href", "/ava/start")
+	}
+	if Global.Start == "start.html" {
+	} else if Global.Start == "login.html" {
+		Global.AutoForm("login", "ava", nil, afterLogin)
+	} else if Global.Start == "register.html" {
+		Global.AutoForm("register", "ava", nil, afterRegister)
+	}
+}
+    
--- markup/play.mu
+++ markup/play.mu
@@ -1,6 +1,6 @@
 div p-0 
   {{ template "navbar_exit" . }}
-  div 
+  div id=board 
     div rounded-tl-lg rounded-tr-lg bg-gradient-to-b from-a-top to-a-bottom h-1/2 w-full
       hi
     div rounded-bl-lg rounded-br-lg bg-yellow-300 h-1/2 w-full
