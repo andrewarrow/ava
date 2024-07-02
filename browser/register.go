5375bc81177683ba64d9017810852afe819470eb
Andrew Arrow <oneone@gmail.com>
2024-07-01 21:29:37 -0700

    updated copyright information

--- browser/register.go
+++ browser/register.go
@@ -30,8 +30,14 @@ func RegisterEvents() {
 		ctx.Set("lineWidth", 2)
 		for i := 0; i < 9; i++ {
 			ctx.Call("beginPath")
-			ctx.Call("moveTo", 18+(i*36), 0)
-			ctx.Call("lineTo", 18+(i*36), height)
+			ctx.Call("moveTo", 18+(i*18), 0)
+			ctx.Call("lineTo", 18+(i*18), height)
+			ctx.Call("stroke")
+		}
+		for i := 0; i < 9; i++ {
+			ctx.Call("beginPath")
+			ctx.Call("moveTo", 0, 18+(i*36))
+			ctx.Call("lineTo", width, 18+(i*36))
 			ctx.Call("stroke")
 		}
 	} else if Global.Start == "login.html" {
